eec353d90bac2cc2bdfa3559667539cae25aaa53
Ankit_Borude <ankitborude250@gmail.com>
2023-01-27 15:33:04 +0530

    Formated

--- FirstProject/src/ai/DFS.java
+++ FirstProject/src/ai/DFS.java
@@ -17,11 +17,9 @@ public class DFS implements IEngine {
 	@Override
 	public void solve(Rules rules, State initialState, State finalState) {
 		State tempState;
-		int counter = 0;
 		stack.push(initialState);
 		while (!stack.isEmpty()) {
 			State cstate, costate = new State(10, 10);
-			System.out.print("" + counter++);
 			tempState = stack.pop();
 			tempState.viewState();
 			System.out.print("  At Depth " + tempState.getDepth());
--- FirstProject/src/ai/Main.java
+++ FirstProject/src/ai/Main.java
@@ -4,10 +4,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		IEngine engine = new DFS(15);
-		Jug jugs = new Jug(4, 3);//size of each jug 
-		Jug.State startingState = new Jug.State(0, 0);//start state
-		Jug.State goalState = new Jug.State(0,2);//goal state
+		IEngine engine = new DFS(15);// attaching object of bfs or dfs engine as required
+		Jug jugs = new Jug(4, 3);// size of each jug
+		Jug.State startingState = new Jug.State(0, 0);// start state
+		Jug.State goalState = new Jug.State(0, 2);// goal state
 		ProductionSystem system = new ProductionSystem(jugs, startingState, goalState, engine);
 	}
 
